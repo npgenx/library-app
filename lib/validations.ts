6c4b917f6c9cfd2e6fb35a5c472fa6635c57b41d
JP Morgan <679434+npgenx@users.noreply.github.com>
2025-08-27 18:10:44 -0700

    changed image requirement until the issue can be addressed

--- lib/validations.ts
+++ lib/validations.ts
@@ -1,11 +1,12 @@
 import { z } from "zod";
 
 export const signUpSchema = z.object({
-  fullName: z.string().min(3),
-  email: z.string().email(),
-  universityId: z.coerce.number(),
-  universityCard: z.string().nonempty("University Card is required"),
-  password: z.string().min(8),
+    fullName: z.string().min(3),
+    email: z.string().email(),
+    universityId: z.coerce.number(),
+    // universityCard: z.string().nonempty('University Card is required'),
+    universityCard: z.string().optional(),
+    password: z.string().min(8),
 });
 
 export const signInSchema = z.object({
